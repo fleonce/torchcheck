6ee12903f07dff9fe971a618f0cb35ec07889c20
Moritz Hennen <moritz.hennen@unibw.de>
2023-11-22 20:49:06 +0100

    Update symbolic evaluation code

--- torchcheck/impl/asserts.py
+++ torchcheck/impl/asserts.py
@@ -6,13 +6,11 @@ _int = Union[int, SymInt]
 
 
 def assert_shape(x: Tensor, shape: tuple[_int, ...]) -> int:
-    print(x.size(), shape)
     sizes = x.size()
     assert len(sizes) == len(shape)
     for i, j in zip(sizes, shape):
-        if isinstance(i, SymInt):
-            print(i, i.node, i.node.shape_env.var_to_val, x, x.size())
-        # print(i, j)
-    assert x.size() == shape, f"Expected shape of tensor to be: {shape} but got: {x.size()}"
+        pass
+    assert x.size() == len(shape), f"Expected shape of tensor to be: {shape} but got: {x.size()}"
+    assert sizes == shape, f"Expected shape of tensor to be: {shape} but got: {x.size()}"
     return 0
 
