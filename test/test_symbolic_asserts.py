3d33c47af1612fc89c31238ae1906cc61b6d62ed
Moritz Hennen <moritz.hennen@unibw.de>
2023-12-30 20:31:07 +0100

    Make error message more verbose

--- test/test_symbolic_asserts.py
+++ test/test_symbolic_asserts.py
@@ -6,6 +6,7 @@ from torch.fx.experimental.symbolic_shapes import ShapeEnv
 from torch._subclasses.fake_tensor import FakeTensorMode, FakeTensor
 from torchcheck import assert_shape, config
 
+
 class SymbolicAssertTestCase(unittest.TestCase):
 
     def test_symbolic_assert(self):
@@ -15,11 +16,12 @@ class SymbolicAssertTestCase(unittest.TestCase):
         t1 = torch.empty((1, 3))
         with enable_python_dispatcher(), fake_env:
             t2 = fake_env.from_tensor(t1)
-            self.assertRaises(
-                RuntimeError,
-                lambda: assert_shape(t2, (1, 3)),
-                msg=f"No RuntimeError thrown by assert_shape(t2, (1, 3)), shape was {t2.shape}"
-            )
+            with self.assertRaises(
+                    RuntimeError,
+                    msg=f"No RuntimeError thrown by assert_shape(t2, (1, 3)), "
+                        f"shape was {t2.shape}"
+            ):
+                assert_shape(t2, (1, 3))
             self.assertRaises(RuntimeError, lambda: assert_shape(t1, (1, t2.size(1))))
             self.assertEqual(assert_shape(t1, (1, 3)), 0)
             self.assertEqual(assert_shape(t2, t2.size()), 0)
